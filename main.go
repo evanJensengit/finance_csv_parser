224ddf8d3f50193c204ad0909625eb06d7e92c7c
Evan Jensen <evan@sirrus7.com>
2023-12-15 14:37:00 -0800

    dates parsed, to get charges in date range

--- main.go
+++ main.go
@@ -26,6 +26,9 @@ type Transaction struct {
 	CharacterPatterns []string
 }
 
+var monthsOfYear = map[string]int{"January": 1, "February:": 2, "March": 3, "April": 4, "May": 5, "June": 6, "July": 7,
+	"Auguest": 8, "September": 9, "October": 10, "November": 11, "December": 12}
+
 const other string = "other"
 
 var ignorePayment = []string{"online", "payment", "thank", "you"}
@@ -126,42 +129,96 @@ func initializeTransactionsAtPlacesMap(keywordMapToPlaces map[string]string) (ma
 	return transactionsAtPlaces, nil
 }
 
-func main() {
-	placesWithPatterns := "placesWithPatterns.txt"
-	placesMappedToPatterns, err := generatePlaceMap(placesWithPatterns)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
+func firstDateLessThanSecondDate(date1 time.Time, date2 time.Time) (bool, error) {
+	fmt.Println("First date: ", date1, "\n", "Second Date: ", date2)
+	if date1.Year() < date2.Year() {
+		return true, nil
+	} else if date1.Year() == date2.Year() {
+		if int(date1.Month()) < int(date2.Month()) {
+			return true, nil
+		} else if int(date1.Month()) == int(date2.Month()) {
+			if date1.Day() < date2.Day() {
+				return true, nil
+			}
+		}
 	}
-	//logging
-	fmt.Println("Keyword map to places")
+	return false, nil
+}
 
-	for key, val := range placesMappedToPatterns {
-		fmt.Println(key, ":", val)
-	}
-	//logging
-	transactionsAtPlacesMap, err := initializeTransactionsAtPlacesMap(placesMappedToPatterns)
+func findTransactionRangeToCalculate(transactionsAtPlaces []Transaction) ([]Transaction, error) {
+	lastTransactionDate := transactionsAtPlaces[0].Date
+	firstTransactionDate := transactionsAtPlaces[len(transactionsAtPlaces)-1].Date
+
+	fmt.Println("Please enter the dates you would like to calculate transactions ",
+		"between the range of ", firstTransactionDate.Day(), firstTransactionDate.Month(), firstTransactionDate.Year(), " to",
+		lastTransactionDate.Day(), lastTransactionDate.Month(), lastTransactionDate.Year(),
+		"in the form of from: mm/dd/yyyy to: mm/dd/yyyy")
+
+	//if fromInput <fromDate err
+	//if toInput > toDate err
+
+	// scanning the input by the user
+	var fromInput, toInput string
+	fmt.Scanln(&fromInput, &toInput)
+	fmt.Println("from date", fromInput)
+	fmt.Println("to date", toInput)
+
+	fromInputDate, err := time.Parse("01/02/2006", fromInput)
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		fmt.Println("Error parsing date:", err)
+		return []Transaction{}, err
 	}
-	//logging
-	fmt.Println("TransactionsAtPlacesMap")
-	for key, val := range transactionsAtPlacesMap {
-		fmt.Println(key, ":", val)
-	}
-	//logging
-	listOfTransactions, err := createTransactionObjects()
+	toInputDate, err := time.Parse("01/02/2006", toInput)
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		fmt.Println("Error parsing date:", err)
+		return []Transaction{}, err
 	}
-	unmatched := [][]string{}
-	calculateTransactionsAtPlaces(transactionsAtPlacesMap, listOfTransactions, placesMappedToPatterns, unmatched)
-	fmt.Println(transactionsAtPlacesMap)
-	//fmt.Printf("%.2f", k) to get rounded 2 decimal places
-	//fmt.Println(data)
-	return
+	inputChronological, err := firstDateLessThanSecondDate(fromInputDate, toInputDate)
+	if err != nil {
+		fmt.Println("Invalid date range entered:", err)
+		return []Transaction{}, err
+	}
+	firstInputLessThanLastTransaction, err := firstDateLessThanSecondDate(fromInputDate, lastTransactionDate)
+	if err != nil {
+		fmt.Println("Invalid date range entered:", err)
+		return []Transaction{}, err
+	}
+
+	fmt.Println("inputChronological", inputChronological, "firstInputLessThanLastTransaction", firstInputLessThanLastTransaction)
+	//most reacent closer to front of life
+
+	if inputChronological && firstInputLessThanLastTransaction {
+		fmt.Println("valid dates entered")
+		back := len(transactionsAtPlaces) - 1
+		front := 0
+		for back > front {
+			inputGreaterThanCurrent, err := firstDateLessThanSecondDate(transactionsAtPlaces[back].Date, fromInputDate)
+			if err != nil {
+				fmt.Println("Invalid date range entered:", err)
+				return []Transaction{}, err
+			}
+			if inputGreaterThanCurrent {
+				back--
+			}
+
+			inputLessThanCurrent, err := firstDateLessThanSecondDate(transactionsAtPlaces[front].Date, toInputDate)
+			if err != nil {
+				fmt.Println("Invalid date range entered:", err)
+				return []Transaction{}, err
+			}
+			if !inputLessThanCurrent {
+				front++
+			}
+
+			if !inputGreaterThanCurrent && inputLessThanCurrent {
+				break
+			}
+			fmt.Println("Back ", back, " Front ", front)
+		}
+		return transactionsAtPlaces[front : back+1], nil
+	}
+	return []Transaction{}, nil
+
 }
 
 func calculateTransactionsAtPlaces(transactionsAtPlacesMap map[string]float64,
@@ -192,7 +249,6 @@ func calculateTransactionsAtPlaces(transactionsAtPlacesMap map[string]float64,
 			fmt.Println("Other Transaction", transaction)
 		}
 	}
-
 }
 
 // creates transactions objects for each row in csv file
@@ -252,4 +308,49 @@ func createTransactionObjects() ([]Transaction, error) {
 	}
 	return data, nil
 	// Print the resulting struct
+}
+
+func main() {
+	placesWithPatterns := "placesWithPatterns.txt"
+	placesMappedToPatterns, err := generatePlaceMap(placesWithPatterns)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	//logging
+	fmt.Println("Keyword map to places")
+
+	for key, val := range placesMappedToPatterns {
+		fmt.Println(key, ":", val)
+	}
+
+	//logging
+	transactionsAtPlacesMap, err := initializeTransactionsAtPlacesMap(placesMappedToPatterns)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	//logging
+	// fmt.Println("TransactionsAtPlacesMap")
+	// for key, val := range transactionsAtPlacesMap {
+	// 	fmt.Println(key, ":", val)
+	// }
+	//logging
+	listOfTransactions, err := createTransactionObjects()
+
+	listOfTransactions, err = findTransactionRangeToCalculate(listOfTransactions)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Transactions: ")
+	fmt.Println(listOfTransactions)
+	unmatched := [][]string{}
+	calculateTransactionsAtPlaces(transactionsAtPlacesMap, listOfTransactions, placesMappedToPatterns, unmatched)
+	fmt.Println(transactionsAtPlacesMap)
+	//fmt.Printf("%.2f", k) to get rounded 2 decimal places
+	//fmt.Println(data)
+	return
 }
